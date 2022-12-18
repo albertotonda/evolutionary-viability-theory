4fdfe1f478f0be9634dc5e5e17a36ef3d2607863
albertotonda <alberto.tonda@gmail.com>
2022-12-18 23:32:14 +0100

    Basic skeleton for the viability theory problem class set up.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# evolutionary-viability-theory
+# Evolutionary Viability Theory
 Repository for the experiments on EAs applied to viability theory
--- src/viabilitytheory.py
+++ src/viabilitytheory.py
@@ -10,21 +10,47 @@ class ViabilityTheoryProblem :
     Class that describes a viability theory problem
     """
 
-    def __init__(self, equations={}, constraints={}, control={}) :
+    def __init__(self, equations={}, constraints={}, parameters={}, control={}) :
 
         # we assume that all ODEs are dX/dt
         self.equations = {}
         for state_variable, equation in equations.items() :
             self.equations[state_variable] = sympy.sympify(equation)
 
-    def __str__() :
+        self.control = control
+
+        # we expect constraints to be a dictionary of lists of strings
+        self.constraints = {}
+        for variable, constraint_list in constraints.items() :
+            self.constraints[variable] = []
+            for c in constraint_list :
+                self.constraints[variable].append( sympy.sympify(c) )
+
+    def __str__(self) :
         """
         Returns class description as a string.
         """
         class_string = "\n"
 
+        class_string += "Equations:\n"
         for state_variable, equation in self.equations.items() :
-            class_string += "d" + state_variable + "/dt = " + str(equation)
+            class_string += "d" + state_variable + "/dt = " + str(equation) + "\n"
+
+        class_string += "\n"
+
+        if len(self.constraints) > 0 :
+            class_string += "Constraints:\n"
+            for variable, constraint_list in self.constraints.items() :
+                for c in constraint_list :
+                    class_string += variable + " : " + str(c) + "\n"
+            class_string += "\n"
+
+        if len(self.control) > 0 :
+            class_string += "Control:\n"
+            for variable, control_equation in self.control.items() :
+                class_string += variable + " = " + str(control_equation)
+                if control_equation == "" :
+                    class_string += "?"
 
         return class_string
         
@@ -35,5 +61,19 @@ if __name__ == "__main__" :
     This part here is just to test the functions.
     """
 
-    # this is the famous "Lake" example of viability theory
+    # this is the famous "Lake eutrophication" example of viability theory
+    # https://demo.vino.openmole.org/viabilityproblem/1/#kernel/1/
+    equations = {
+            "L" : "u",
+            "P" : "-b * P + L + r * P**q/(m**q + P**q)"
+            }
+    control = {"u" : ""}
+    constraints = {
+            "u" : ["u >= umin", "u <= umax"],
+            "L" : ["L >= Lmin", "L <= Lmax"],
+            "P" : ["P >= 0", "P <= Pmax"],
+            }
+
+    vp = ViabilityTheoryProblem(equations=equations, control=control, constraints=constraints)
+    print(vp)
 
